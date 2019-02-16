75944b941674f8ee7cc1356c800285c87f31a83d
Bonnie Jordan <thebonstergirl@gmail.com>
2019-02-16 15:11:21 -0600

    initial commit

---
+++ src/server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { json } = require("body-parser");
+const firebase = require("firebase");
+require("firebase/database");
+require("dotenv").config();
+
+const app = express();
+app.use(json());
+
+var config = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET
+  };
+
+firebase.initializeApp(config);
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port} ^___^!`);
+});
